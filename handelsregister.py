7b51160200fbf4998b52b1da8d70fe97bd581143
Achille Nana Chimi <me@nanachimi.com>
2023-01-08 05:09:12 +0100

    enhancement in progress

--- handelsregister.py
+++ handelsregister.py
@@ -5,10 +5,10 @@ You can query, download, automate and much more, without using a web browser.
 """
 
 import argparse
-import mechanize
-import re
 import pathlib
 import sys
+
+import mechanize
 from bs4 import BeautifulSoup
 
 # Dictionaries to map arguments to values
@@ -18,13 +18,16 @@ schlagwortOptionen = {
     "exact": 3
 }
 
-class HandelsRegister:
-    def __init__(self, args):
-        self.args = args
+
+class GermanTradeRegister:
+    def __init__(self, search_query, debug: bool, cache: bool):
+        self.search_args = search_query
+        self.debug_mode = debug
+        self.cache_mode = cache
         self.browser = mechanize.Browser()
 
-        self.browser.set_debug_http(args.debug)
-        self.browser.set_debug_responses(args.debug)
+        self.browser.set_debug_http(debug)
+        self.browser.set_debug_responses(debug)
         # self.browser.set_debug_redirects(True)
 
         self.browser.set_handle_robots(False)
@@ -39,150 +42,223 @@ class HandelsRegister:
                 "User-Agent",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15",
             ),
-            (   "Accept-Language", "en-GB,en;q=0.9"   ),
-            (   "Accept-Encoding", "gzip, deflate, br"    ),
+            ("Accept-Language", "de-DE,en;q=0.9"),
+            ("Accept-Encoding", "gzip, deflate, br"),
             (
                 "Accept",
                 "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             ),
-            (   "Connection", "keep-alive"    ),
+            ("Connection", "keep-alive"),
         ]
-        
-        self.cachedir = pathlib.Path("cache")
-        self.cachedir.mkdir(parents=True, exist_ok=True)
+
+        self.cache_dir = pathlib.Path("cache")
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
 
     def open_startpage(self):
         self.browser.open("https://www.handelsregister.de", timeout=10)
 
-    def companyname2cachename(self, companyname):
-        # map a companyname to a filename, that caches the downloaded HTML, so re-running this script touches the
-        # webserver less often.
-        return self.cachedir / companyname
+    def cache_file_name(self, search_query_hash):
+        return self.cache_dir / str(search_query_hash)
+
+    def search_query_identifier(self):
+        d = vars(self.search_args)
+        query_identifier = ''
+
+        for key, value in d.items():
+            query_identifier += key + ":" + str(value) + "_"
+
+        query_identifier = query_identifier[:-1]
+        query_identifier = query_identifier.lower() + ".html"
+
+        return query_identifier
 
     def search_company(self):
-        cachename = self.companyname2cachename(self.args.schlagwoerter)
-        if self.args.force==False and cachename.exists():
-            with open(cachename, "r") as f:
+        search_query_identifier = self.search_query_identifier()
+        cache_file_name = self.cache_file_name(search_query_identifier)
+
+        if self.cache_mode and cache_file_name.exists():
+            with open(cache_file_name, "r") as f:
                 html = f.read()
-                print("return cached content for %s" % self.args.schlagwoerter)
+                print("return cached content for %s" % self.search_args.schlagwoerter)
         else:
             # TODO implement token bucket to abide by rate limit
             # Use an atomic counter: https://gist.github.com/benhoyt/8c8a8d62debe8e5aa5340373f9c509c7
-            response_search = self.browser.follow_link(text="Advanced search")
+            self.browser.follow_link(text="Erweiterte Suche")
 
-            if self.args.debug == True:
+            if self.debug_mode:
                 print(self.browser.title())
 
             self.browser.select_form(name="form")
 
-            self.browser["form:schlagwoerter"] = self.args.schlagwoerter
-            so_id = schlagwortOptionen.get(self.args.schlagwortOptionen)
+            self.browser["form:schlagwoerter"] = self.search_args.schlagwoerter
 
+            so_id = schlagwortOptionen.get(self.search_args.schlagwortOptionen)
             self.browser["form:schlagwortOptionen"] = [str(so_id)]
+
+            ergebnisse_pro_seite = [self.search_args.ergebnisseProSeite]
+            self.browser["form:ergebnisseProSeite_input"] = ergebnisse_pro_seite
+
+            self.browser["form:NiederlassungSitz"] = self.search_args.niederlassung
+
+            register_art = [self.search_args.registerArt]
+            self.browser["form:registerArt_input"] = register_art
+
+            register_gericht = [self.search_args.registerGericht]
+            self.browser["form:registergericht_input"] = register_gericht
+
+            self.browser["form:ort"] = self.search_args.ort
 
             response_result = self.browser.submit()
 
-            if self.args.debug == True:
+            if self.debug_mode:
                 print(self.browser.title())
 
             html = response_result.read().decode("utf-8")
-            with open(cachename, "w") as f:
+            with open(cache_file_name, "w") as f:
                 f.write(html)
 
             # TODO catch the situation if there's more than one company?
             # TODO get all documents attached to the exact company
             # TODO parse useful information out of the PDFs
-        return get_companies_in_searchresults(html)
+        return extract_companies_in_search_results(self, html)
 
 
-def parse_result(result):
+def parse_result(self, result):
     cells = []
-    for cellnum, cell in enumerate(result.find_all('td')):
-        #print('[%d]: %s [%s]' % (cellnum, cell.text, cell))
+
+    for cell_num, cell in enumerate(result.find_all('td')):
         cells.append(cell.text.strip())
-    #assert cells[7] == 'History'
-    d = {}
-    d['court'] = cells[1]
-    d['name'] = cells[2]
-    d['state'] = cells[3]
-    d['status'] = cells[4]
-    d['documents'] = cells[5] # todo: get the document links
-    d['history'] = []
-    hist_start = 8
-    hist_cnt = (len(cells)-hist_start)/3
-    for i in range(hist_start, len(cells), 3):
-        d['history'].append((cells[i], cells[i+1])) # (name, location)
-    #print('d:',d)
-    return d
 
-def pr_company_info(c):
-    for tag in ('name', 'court', 'state', 'status'):
-        print('%s: %s' % (tag, c.get(tag, '-')))
+    # current_register_printout = self.browser.find_link(text='AD')
+
+    search_result = {'court': cells[1],
+                     'name': cells[2],
+                     'city': cells[3],
+                     'active': cells[4] == ("aktuell" or "currently registered"),
+                     'documents': cells[5],  # TODO: retrieve all document links or binaries
+                     'history': []
+                     }
+    history_start_index = 8
+    history_end_index = len(cells)
+    history_index = 1
+    for i in range(history_start_index, history_end_index, 3):
+        history_prefix = str(history_index) + ".) "
+        if not cells[i].startswith(history_prefix):
+            break
+        if i + 1 == history_end_index:
+            break
+        if not cells[i + 1].startswith(history_prefix):
+            break
+        name = cells[i].replace(history_prefix, "")
+        city = cells[i + 1].replace(history_prefix, "")
+        history = {"position": history_index, "name": name, "city": city}
+        search_result['history'].append(history)
+        history_index += 1
+    return search_result
+
+
+def print_company_info(company):
+    for tag in ('name', 'court', 'city', 'active'):
+        print('%s: %s' % (tag, company.get(tag, '-')))
     print('history:')
-    for name, loc in c.get('history'):
-        print(name, loc)
+    for history in company.get('history'):
+        print("position: ", history['position'])
+        print("name: ", history['name'])
+        print("city: ", history['city'])
+    print("-------------------------------")
 
-def get_companies_in_searchresults(html):
+
+def extract_companies_in_search_results(self, html):
     soup = BeautifulSoup(html, 'html.parser')
     grid = soup.find('table', role='grid')
-    #print('grid: %s', grid)
-  
+    # self.browser.find_link(text='AD')
+
     results = []
     for result in grid.find_all('tr'):
         a = result.get('data-ri')
         if a is not None:
-            index = int(a)
-            #print('r[%d] %s' % (index, result))
-            d = parse_result(result)
+            if self.debug_mode:
+                index = int(a)
+                print('r[%d] %s' % (index, result))
+            d = parse_result(self, result)
             results.append(d)
     return results
 
-def parse_args():
-# Parse arguments
-    parser = argparse.ArgumentParser(description='A handelsregister CLI')
-    parser.add_argument(
-                          "-d",
-                          "--debug",
-                          help="Enable debug mode and activate logging",
-                          action="store_true"
-                        )
-    parser.add_argument(
-                          "-f",
-                          "--force",
-                          help="Force a fresh pull and skip the cache",
-                          action="store_true"
-                        )
-    parser.add_argument(
-                          "-s",
-                          "--schlagwoerter",
-                          help="Search for the provided keywords",
-                          required=True,
-                          default="Gasag AG" # TODO replace default with a generic search term
-                        )
-    parser.add_argument(
-                          "-so",
-                          "--schlagwortOptionen",
-                          help="Keyword options: all=contain all keywords; min=contain at least one keyword; exact=contain the exact company name.",
-                          choices=["all", "min", "exact"],
-                          default="all"
-                        )
-    args = parser.parse_args()
 
+def parse_args(debug_mode: bool):
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='A handelsregister CLI')
+
+    parser.add_argument(
+        "-s",
+        "--schlagwoerter",
+        help="Search for the provided keywords",
+        required=False
+    )
+
+    parser.add_argument(
+        "-so",
+        "--schlagwortOptionen",
+        choices=["all", "min", "exact"],
+        default="min"
+    )
+
+    parser.add_argument(
+        "-eps",
+        "--ergebnisseProSeite",
+        default="100"
+    )
+
+    parser.add_argument(
+        "-nl",
+        "--niederlassung",
+        default="Mannheim"
+    )
+
+    parser.add_argument(
+        "-ra",
+        "--registerArt",
+        choices=["alle", "HRA", "HRB", "GnR", "PR", "VR"],
+        default="HRB"
+    )
+
+    parser.add_argument(
+        "-rg",
+        "--registerGericht"
+    )
+
+    parser.add_argument(
+        "-rf",
+        "--rechtsform"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--ort",
+        default="Mannheim"
+    )
+
+    # search_args = parser.parse_args()
+    # print(type(search_args))
+    # debug_arg = search_args.debug
 
     # Enable debugging if wanted
-    if args.debug == True:
+    if debug_mode:
         import logging
         logger = logging.getLogger("mechanize")
         logger.addHandler(logging.StreamHandler(sys.stdout))
         logger.setLevel(logging.DEBUG)
 
-    return args
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    args = parse_args()
-    h = HandelsRegister(args)
-    h.open_startpage()
-    companies = h.search_company()
+    debug_mode = False
+    cache_mode = False
+    search_args = parse_args(debug_mode)
+    trade_register = GermanTradeRegister(search_args, debug_mode, cache_mode)
+    trade_register.open_startpage()
+    companies = trade_register.search_company()
     if companies is not None:
         for c in companies:
-            pr_company_info(c)
+            print_company_info(c)
